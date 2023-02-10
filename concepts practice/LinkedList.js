d884e7a1269ee62648800f198939d0408cd69973
Ericawanja <wanjaerica@gmail.com>
2023-02-10 10:21:13 +0300

    insert At pos in linked list

--- concepts practice/LinkedList.js
+++ concepts practice/LinkedList.js
@@ -28,13 +28,41 @@ class LinkedList {
     }
     return lastnode;
   }
-  add(value) {
+  insertAtEnd(value) {
     const node = new Node(value);
     let pointer = this.head;
     while (pointer.next) {
-        pointer = pointer.next
+      pointer = pointer.next;
     }
-    pointer.next = value
+    pointer.next = node;
+    return this.head;
+  }
+  insertAtBeginning(value) {
+    const node = new Node(value);
+    node.next = this.head;
+    this.head = node;
+    return this.head;
+  }
+  insertAtPos(value, pos) {
+    if (pos < 0 || pos > this.size()) return "invalid position";
+    if (pos === 0) return this.insertAtBeginning(value);
+    if (pos === this.size() - 1) return this.insertAtEnd(value);
+
+    //inserting at middle
+
+    let node = new Node(value);
+
+    let current = this.head;
+    let index = 0;
+    let previous;
+    while (index < pos) {
+      previous = current;
+      current = current.next;
+      index++;
+    }
+    node.next = current;
+    previous.next = node;
+    return this.head;
   }
 }
 
@@ -44,6 +72,9 @@ let node2 = new Node(3);
 node1.next = node2;
 let list = new LinkedList(node1);
 // console.log(node1, list);
-list.add(9)
+list.insertAtEnd(9);
 
-console.log(list.size(), list);
+// console.log(list.size());
+console.log(list.insertAtBeginning(1));
+// console.log(list.getLastNode())
+console.log(list.insertAtPos(106, 0))
